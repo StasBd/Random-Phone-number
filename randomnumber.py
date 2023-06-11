ca81725047082361d159e7c860797535946f1717
Stanislav <129835954+StasBd@users.noreply.github.com>
2023-06-11 13:00:07 +0300

    Add files via upload

---
+++ randomnumber.py
@@ -0,0 +1,19 @@
+
+
+numbers = "1234567890"
+
+import random
+
+def createnumber():
+    N1 = random.choice(numbers)
+    N2 = random.choice(numbers)
+    N3 = random.choice(numbers)
+    N4 = random.choice(numbers)
+    N5 = random.choice(numbers)
+    N6 = random.choice(numbers)
+    N7 = random.choice(numbers)
+    N8 = random.choice(numbers)
+    N9 = random.choice(numbers)
+    N10 = random.choice(numbers)
+    print("Youre new number is:", '+'+N1+N2+N3+N4+N5+N6+N7+N8+N9+N10)
+createnumber()
